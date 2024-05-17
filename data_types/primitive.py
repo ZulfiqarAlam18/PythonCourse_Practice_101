dc13f5467f704cedfe9e4faf1c5c60aeccdbc4e3
Zulfiqar Alam <zulfi18.official@gmail.com>
2024-05-17 12:33:52 -0400

    Day one Progress

---
+++ data_types/primitive.py
@@ -0,0 +1,10 @@
+
+a = 10 #integer
+b = 10.3 #floating
+c = 'c'  #character
+d = True  #boolean
+e = 2+3j #Complex number
+f = "Zulfiqar Alam"  #Strings
+g = b"hellowordldksfajf"  #byte data (stream of 8)
+
+print(g)
