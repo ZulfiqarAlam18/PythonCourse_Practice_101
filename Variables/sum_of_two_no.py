13590f0050ad4f07e52e69be01c3ed6f9db6317c
Zulfiqar Alam <zulfi18.official@gmail.com>
2024-05-18 10:49:01 -0400

    day two progress

---
+++ Variables/sum_of_two_no.py
@@ -0,0 +1,7 @@
+
+
+num1 = int(input("Enter Number 1 :"))
+num2 = int(input("Enter Number 2 :"))
+
+print("The sum is :",num1+num2)
+print(f"The sum is : {num1+num2}")
---
+++ Variables/variables.py
@@ -0,0 +1,3 @@
+#Containers inside which data is stored
+
+x = 20 #here x is a variable or in simple terms we can say a memory location occupied with name x
