c182bc04ec7fea7dcc7591388477075708e94a2b
Zulfiqar Alam <zulfi18.official@gmail.com>
2024-05-19 23:51:43 -0400

    day 3 progess updated

---
+++ Strings/counting_specsifc_char.py
@@ -0,0 +1,8 @@
+
+str = "My Name is Lackan and i live in india Aisa and yes i do like manaoes"
+
+char = "a"
+# char = 'a'  for better practice cause we looking for a specific character not a string
+count = str.count(char)
+
+print(count)
---
+++ Strings/finding_specific_char.py
@@ -0,0 +1,11 @@
+
+
+str = "My name is lackan , and i live in England "
+
+find = "z"
+
+count = str.count(find)
+if count == 0 :
+    print("Not Found")
+else:
+    print("Found")
