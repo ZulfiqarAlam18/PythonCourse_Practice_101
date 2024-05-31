c2b9083c12e60b8776a4850ff4f65bf1da498dac
Zulfiqar Alam <zulfi18.official@gmail.com>
2024-05-31 10:22:05 -0400

    Day 5th ends here

---
+++ Touples/list_to_tuple.py
@@ -0,0 +1,15 @@
+
+myList = [10,20,30,40,50,60,70,80]
+
+print(type(myList))
+
+tupled_data = tuple(myList)
+
+print(type(tupled_data))
+
+
+#Vice Versa 
+
+tuple_into_list = list(tupled_data)
+
+print(type(tuple_into_list))
---
+++ Touples/searching.py
@@ -0,0 +1,16 @@
+
+animals = ("Dog","Lion","Dragon","Cat")
+
+print("Dog" in animals)
+
+marks = (44,55,80,98)
+
+print(44 in marks)
+
+
+#Another way
+
+for i in marks :
+    if i == 44 :
+        print("Found")
+        break
